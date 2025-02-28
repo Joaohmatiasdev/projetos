2524c954bff71667b556dd05f509348510005105
Joaohmatiasdev <Joaohenrimatias@gmail.com>
2025-02-28 15:42:50 -0300

    jvs
    Imc em javascript

--- 02/devs.js
+++ 02/devs.js
@@ -1,9 +1,15 @@
-let nome = prompt('Qual é seu nome?')
-let idade = Number(prompt( nome + 'Qual é sua idade?'))
-let altura = Number(prompt( nome +  'Qual é sua atura? '))
-let peso = Number(prompt(nome + 'Qual é seu peso?'))
+let nome = prompt('qual é seu nome?')
+let idade = Number(prompt( nome + ' qual é sua idade?'))
+let altura = Number(prompt( nome +  ' qual é sua altura? '))
+let peso = Number(prompt(nome + ' qual é seu peso?'))
+
+let AnosNasc = 0
+
+AnosNasc = 2023 - idade
 
 
 let imc = peso / (altura * altura)
 
 console.log(imc)
+
+console.log('Ola ' +  nome, ' você tem ' + idade + ' anos, ' + ' nasceu em  ' + AnosNasc,  ' tem ' +  altura  + ' de altura,pesa ' + peso +'kg seu IMC é ' + imc + 'kg/m2')
